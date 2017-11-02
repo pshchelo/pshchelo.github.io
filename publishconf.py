745f21a2a244e74d8f7fc894a34d7b35918902f9
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2017-11-02 10:07:31 +0200

    Move the blog to https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://pshchelo.github.io'
+SITEURL = 'https://pshchelo.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
